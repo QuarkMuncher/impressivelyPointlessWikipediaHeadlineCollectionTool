b855fac7b78ef0c8bdbc81609d26d50bcb70635d
Sebastian Andersen <sebastian@sandersen.co>
2019-06-06 11:16:41 +0200

    Adds, very simpel error handling and formats code.

--- main.py
+++ main.py
@@ -3,34 +3,56 @@ import requests as forespørgsler
 from bs4 import BeautifulSoup as SmukSuppe
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 import settings as s
+from colorama import Fore, init
 
-engine = create_engine(f"{s.DB_DRIVER}://{s.DB_USER}:{s.DB_PASSWORD}@{s.DB_SERVER}/{s.DB_NAME}", echo = True)
+init()
+
+engine = create_engine(
+    f"{s.DB_DRIVER}://{s.DB_USER}:{s.DB_PASSWORD}@{s.DB_SERVER}/{s.DB_NAME}"
+)
 meta = MetaData()
 wikilist = Table(
-    'wikilist', meta,
-    Column('id', Integer, primary_key = True),
-    Column('headline', String(100))
+    "wikilist",
+    meta,
+    Column("id", Integer, primary_key=True),
+    Column("headline", String(100)),
 )
 
+
 def whut():
-    resultat = forespørgsler.get('https://en.wikipedia.org/wiki/Special:Random')
-    suppe = SmukSuppe(resultat.content, 'html.parser')
+    resultat = forespørgsler.get(
+        "https://en.wikipedia.org/wiki/Special:Random"
+    )
+    suppe = SmukSuppe(resultat.content, "html.parser")
     kondenseret_suppe = suppe.h1
-    
+
     if kondenseret_suppe.i:
-        loven = kondenseret_suppe.decode_contents().replace("<i>", "").replace("</i>", "").replace('&amp;', 'og') + " er ikke nice"
+        loven = (
+            kondenseret_suppe.decode_contents()
+            .replace("<i>", "")
+            .replace("</i>", "")
+            .replace("&amp;", "og")
+            + " er ikke nice"
+        )
         conn = engine.connect()
-        ins = wikilist.insert().values(headline = loven)
+        ins = wikilist.insert().values(headline=loven)
         conn.execute(ins)
-        print(loven)
     else:
-        loven = suppe.h1.decode_contents().replace('&amp;', 'og') + " er nice"
+        loven = suppe.h1.decode_contents().replace("&amp;", "og") + " er nice"
         conn = engine.connect()
-        ins = wikilist.insert().values(headline = loven)
+        ins = wikilist.insert().values(headline=loven)
         conn.execute(ins)
-        print(loven)
 
 
-while(True):
-    whut()
-    tid.sleep( 3 )
+while True:
+    try:
+        whut()
+        print(Fore.GREEN + "--------------------------------")
+        print(Fore.GREEN + "|   https request Succeeded    |")
+        print(Fore.GREEN + "--------------------------------")
+    except Exception:
+        print(Fore.RED + "--------------------------------")
+        print(Fore.RED + "|   https request failed       |")
+        print(Fore.RED + "--------------------------------")
+
+    tid.sleep(3)
--- transportToDb.py
+++ transportToDb.py
@@ -7,12 +7,16 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 from sqlalchemy_utils import database_exists, create_database
 import settings as s
 
-engine = create_engine(f"{s.DB_DRIVER}://{s.DB_USER}:{s.DB_PASSWORD}@{s.DB_SERVER}/{s.DB_NAME}", echo = True)
+engine = create_engine(
+    f"{s.DB_DRIVER}://{s.DB_USER}:{s.DB_PASSWORD}@{s.DB_SERVER}/{s.DB_NAME}",
+    echo=True,
+)
 meta = MetaData()
 wikilist = Table(
-    'wikilist', meta,
-    Column('id', Integer, primary_key = True),
-    Column('headline', String(100))
+    "wikilist",
+    meta,
+    Column("id", Integer, primary_key=True),
+    Column("headline", String(100)),
 )
 
 if not database_exists(engine.url):
@@ -25,7 +29,7 @@ f_list = f.read().splitlines()
 print(f_list[1])
 conn = engine.connect()
 for l in f_list:
-    ins = wikilist.insert().values(headline = l)
+    ins = wikilist.insert().values(headline=l)
     # ins.bind(conn)
     ins.compile()
     conn.execute(ins)
